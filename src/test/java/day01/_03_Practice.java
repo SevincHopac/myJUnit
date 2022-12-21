09f56b4c20cc538466199d547b17bac57efc74d3
Sevinc Hopac <sevinckurtgozlu@gmail.com>
2022-12-21 23:04:03 +0100

    21/12/2022

--- src/test/java/day01/_03_Practice.java
+++ src/test/java/day01/_03_Practice.java
@@ -1,7 +1,10 @@
 package day01;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -16,8 +19,10 @@ public class _03_Practice {
 
         // https://www.google.com/ adresine gidin
         driver.get("https://www.google.com/");
+        driver.findElement(By.xpath("//*[text()='Tümünü reddet']")).click();
 
         // Sayfa basliginin "Google" ifadesi icerdigini test edin
+
         String title = driver.getTitle();
         if (title.contains("Google")){
             System.out.println("Title Test Passed");
@@ -28,13 +33,31 @@ public class _03_Practice {
 
         // Arama cubuguna "Nutella" yazip aratin
 
+        driver.findElement(By.xpath("//input[@title='Ara']")).sendKeys("Nutella", Keys.ENTER);
 
 
 
         // Bulunan sonuc sayisini yazdirin
-        // sonuc sayisinin 10 milyon'dan fazla oldugunu test edin
-        // Sayfayi kapatin
+        WebElement resultText = driver.findElement(By.xpath("//div[@id='result-stats']"));
+        String text = resultText.getText();
 
+        String[] textArr = text.split(" ");
+
+        String resultString = textArr[1];
+        resultString=resultString.replace(".","");
+
+        int resultNum = Integer.parseInt(resultString);
+
+        System.out.println("Number of Result = " + resultNum);
+
+        // sonuc sayisinin 10 milyon'dan fazla oldugunu test edin
+
+        if (resultNum>10000000){
+            System.out.println("Test Passed");
+        }else System.out.println("Test Failed");
+
+        // Sayfayi kapatin
+        driver.close();
 
     }
 }
